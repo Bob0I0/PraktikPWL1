10f608b54a5c89c3da9ea0e247aad0aede54b099
Gresia22 <160713285+Gresia22@users.noreply.github.com>
2024-03-06 20:38:59 +0700

    membuat komentar
    membuat komentar

---
+++ resources/views/komentar.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="logincss.css">
+</head>
+<body>
+    <script src="javascript.js"></script>
+
+    
+    <br><br><br>
+   
+<div class="kotak_login">
+    <h2>Login</h2> 
+    <form action="home.html" method="post">
+        <label for="nim">NIM: </label>
+        <input type="text" id="nim" class="input" placeholder="NIM" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" class="input" placeholder="password" required>
+        <br>
+        <input type="submit" class="tombol_login" value="Login">
+</div>
+
+    </form> 
+    -
+<!-- Buku ini berisi tentang fitur-fitur --> 
+</body>
